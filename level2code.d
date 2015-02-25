38563f8bd861271f8a5b001d6bfc2c8c0a09d3b1
Cat Felts <feltsc@students.wwu.edu>
2015-02-25 14:55:20 -0800

    completed level2!(:

---
+++ level2code.d
@@ -0,0 +1,11 @@
+
+level2code.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	c7 05 20 51 05 08 d2 	movl   $0x7aebe9d2,0x8055120
+   7:	e9 eb 7a 
+   a:	68 94 8c 04 08       	push   $0x8048c94
+   f:	c3                   	ret    
