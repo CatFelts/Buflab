0faf92466c4cb296abb7d8567b5d250337c196f6
Cat Felts <feltsc@students.wwu.edu>
2015-02-25 18:50:42 -0800

    figured out level3!!! on to level4...

---
+++ level3code.d
@@ -0,0 +1,11 @@
+
+level3code.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 d2 e9 eb 7a       	mov    $0x7aebe9d2,%eax
+   5:	bd f0 5f 68 55       	mov    $0x55685ff0,%ebp
+   a:	68 10 8e 04 08       	push   $0x8048e10
+   f:	c3                   	ret    
