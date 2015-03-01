c558c47f684493b45086b1ea3f2d6bfc56dcfd23
Cat Felts <feltsc@students.wwu.edu>
2015-02-28 17:28:19 -0800

    Finished the level 4!(:

---
+++ level4code.d
@@ -0,0 +1,12 @@
+
+level4code.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 d2 e9 eb 7a       	mov    $0x7aebe9d2,%eax
+   5:	29 e5                	sub    %esp,%ebp
+   7:	01 e5                	add    %esp,%ebp
+   9:	68 9b 8d 04 08       	push   $0x8048d9b
+   e:	c3                   	ret    
